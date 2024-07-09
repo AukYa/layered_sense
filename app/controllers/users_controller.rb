2f25c4e8e331251e068350c293a455a77cf7f9c9
AukYa <162345538+AukYa@users.noreply.github.com>
2024-07-09 12:22:07 +0000

    [Add]userのshowページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :ensure_guest_user, only: [:edit]
-  
+
   def index
   end
 
@@ -9,17 +9,22 @@ class UsersController < ApplicationController
     if @user != @user.guest_user?
       @user = current_user
     end
+    @works = @user.works
   end
 
   def edit
   end
-  
+
   private
-  
+
   def ensure_guest_user
     @user = current_user
     if @user.guest_user?
       redirect_to user_path(current_user), notice: "guestuserはプロフィール編集画面へ遷移できません。"
     end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
   end
 end
