2cfce59f868d38b5affebddf02db69b5ae9d337a
AukYa <162345538+AukYa@users.noreply.github.com>
2024-07-10 02:12:11 +0000

    [Add]userの編集機能

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
   end
-  resources :users
+  resources :users, only: [:index, :show, :edit, :update]
   resources :works
   get 'homes/top'
   get 'homes/about', to: 'homes#about', as: :about
