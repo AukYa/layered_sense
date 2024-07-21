058b73e4e7de1e359594233626f43f0ee790f08c
AukYa <162345538+AukYa@users.noreply.github.com>
2024-07-21 10:14:59 +0000

    [Add]ユーザー一覧ページ, 各一覧リンク

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,13 @@ class UsersController < ApplicationController
   before_action :ensure_guest_user, only: [:edit]
 
   def index
+    @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
     if @user == @user.guest_user?
-      @user = guestuser
+      @user = guestuserexit
     end
     @works = @user.works
   end
