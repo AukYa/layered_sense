be335a7f5ddcdc70b19f0e93313cf20238bcdb0b
AukYa <162345538+AukYa@users.noreply.github.com>
2024-07-27 08:06:14 +0000

    [Fix]コメント機能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,22 +2,22 @@ class CommentsController < ApplicationController
   def create
     @work = Work.find(params[:work_id])
     @comment = Comment.new(comment_params)
-    @comment.work_id = @work_id
+    @comment.work_id = @work.id
     @comment.user_id = current_user.id
     @comment.save
     redirect_to request.referer
   end
-  
+
   def destroy
     @work = Work.find(params[:work_id])
     @work_comments = @work.comments
     Comment.find_by(id: params[:id], work_id: params[:work_id]).destroy
     redirect_to request.referer
   end
-  
+
   private
-  
+
   def comment_params
-    params.require(:comment).permit(:content, :each_file)
+    params.require(:comment).permit(:content)
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :work
   belongs_to :user
   
-  has_one_attached :each_file
-  
   validates :content, presence: true
 end
